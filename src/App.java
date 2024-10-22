4df72b0289be7e46def9ee4df22e74f6f01cac11
Pedro <pedroesnarriaga@gmail.com>
2024-10-22 17:49:35 -0400

    teste

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         livroView livroView = new livroView();
         livroView.menuInicial();
+        System.out.println("teste");
     }
 }
--- src/br/controller/livroController.java
+++
@@ -1,5 +0,0 @@
-package br.controller;
-
-class livroController {
-    
-}
